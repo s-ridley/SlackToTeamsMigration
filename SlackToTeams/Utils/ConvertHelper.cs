7b2abcb0470c6505c496db11f6504883d8e8a8fd
Stephen Ridley <stephen.ridley@alcidion.com>
2024-06-18 14:08:35 +1200

    Improved reaction to emoji mapping

--- SlackToTeams/Utils/ConvertHelper.cs
+++ SlackToTeams/Utils/ConvertHelper.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Globalization;
+using System.Reflection;
+using System.Text.RegularExpressions;
 using Serilog;
 using SlackToTeams.Models;
 
@@ -87,56 +89,90 @@ namespace SlackToTeams.Utils {
             string? result = null;
 
             if (!string.IsNullOrWhiteSpace(slackReaction)) {
-                result = slackReaction switch {
-                    "+1" => Emoji.ThumbsUp,
-                    "bed" => Emoji.Bed,
-                    "bookmark" => Emoji.Bookmark,
-                    "burrito" => Emoji.Burrito,
-                    "clap" => Emoji.ClappingHands,
-                    "coffee" => Emoji.HotBeverage,
-                    "cop" => Emoji.PoliceOfficer,
-                    "confused" => Emoji.ConfusedFace,
-                    "cripes" => Emoji.AstonishedFace,
-                    "dark_sunglasses" or "sunglasses" => Emoji.Sunglasses,
-                    "grimacing" => Emoji.GrimacingFace,
-                    "grinning" => Emoji.GrinningFace,
-                    "japanese_goblin" => Emoji.Goblin,
-                    "joy" => Emoji.FaceWithTearsOfJoy,
-                    "mag" => Emoji.MagnifyingGlassTiltedLeft,
-                    "man-bowing" => Emoji.Bowling,
-                    "mantelpiece_clock" => Emoji.MantelpieceClock,
-                    "metal" => Emoji.SignOfTheHorns,
-                    "money_mouth_face" => Emoji.MoneyMouthFace,
-                    "ok_hand" => Emoji.OKHand,
-                    "oncoming_police_car" => Emoji.OncomingPoliceCar,
-                    "parrot" or "party_parrot" => Emoji.Parrot,
-                    "relieved" => Emoji.RelievedFace,
-                    "rocket" => Emoji.Rocket,
-                    "rolling_on_the_floor_laughing" => Emoji.RollingOnTheFloorLaughing,
-                    "sob" => Emoji.LoudlyCryingFace,
-                    "slick" => Emoji.SmilingFaceWithSunglasses,
-                    "slightly_smiling_face" => Emoji.SlightlySmilingFace,
-                    "smile" or "simple_smile" => Emoji.SmilingFace,
-                    "smirk" or "vee" => Emoji.SmirkingFace,
-                    "smith" => Emoji.Hammer,
-                    "ssh" => Emoji.ShushingFace,
-                    "stuck_out_tongue_winking_eye" => Emoji.WinkingFaceWithTongue,
-                    "sun" => Emoji.Sun,
-                    "suspect" => Emoji.FaceWithRaisedEyebrow,
-                    "star" => Emoji.Star,
-                    "tada" => Emoji.MagicWand,
-                    "tophat" => Emoji.TopHat,
-                    "toot" => Emoji.PartyingFace,
-                    "trollin" => Emoji.GrinningSquintingFace,
-                    "turtle" => Emoji.Turtle,
-                    "unamused" => Emoji.UnamusedFace,
-                    "upside_down_face" => Emoji.UpsideDownFace,
-                    "wave" => Emoji.WavingHand,
-                    "white_check_mark" => Emoji.CheckBoxWithCheck,
-                    "zzz" => Emoji.Zzz,
-                    _ => slackReaction,
-                };
-                if (result.Equals(slackReaction)) {
+                TextInfo currentTextInfo = CultureInfo.CurrentCulture.TextInfo;
+
+                string processedReaction = slackReaction.Replace("_", string.Empty);
+                /*
+                string processedReaction = string.Empty;
+
+                string[] reactionArray = slackReaction.Split('_');
+                if (
+                    reactionArray != null &&
+                    reactionArray.Length > 0
+                ) {
+                    foreach (string reaction in reactionArray) {
+                        processedReaction += currentTextInfo.ToTitleCase(reaction);
+                    }
+                }
+                */
+                // Check for exact match
+                foreach (FieldInfo field in typeof(Emoji).GetFields().Where(f => f.Name.Equals(processedReaction, StringComparison.CurrentCultureIgnoreCase))) {
+                    object? rawObject = field.GetRawConstantValue();
+                    if (rawObject != null) {
+                        result = rawObject.ToString();
+                    }
+                }
+
+                // Check for faces
+                if (string.IsNullOrWhiteSpace(result)) {
+                    foreach (FieldInfo field in typeof(Emoji).GetFields().Where(f => f.Name.Equals($"{processedReaction}Face", StringComparison.CurrentCultureIgnoreCase))) {
+                        object? rawObject = field.GetRawConstantValue();
+                        if (rawObject != null) {
+                            result = rawObject.ToString();
+                        }
+                    }
+                }
+
+                // Check for starts with
+                if (string.IsNullOrWhiteSpace(result)) {
+                    foreach (FieldInfo field in typeof(Emoji).GetFields().Where(f => f.Name.StartsWith(processedReaction, StringComparison.CurrentCultureIgnoreCase))) {
+                        object? rawObject = field.GetRawConstantValue();
+                        if (rawObject != null) {
+                            result = rawObject.ToString();
+                        }
+                    }
+                }
+
+                if (string.IsNullOrWhiteSpace(result)) {
+                    result = slackReaction switch {
+                        "+1" => Emoji.ThumbsUp,
+                        "cake" => Emoji.BirthdayCake,
+                        "clap" => Emoji.ClappingHands,
+                        "coffee" or "coffee2" => Emoji.HotBeverage,
+                        "cold_sweat" => Emoji.SweatDroplets,
+                        "cop" => Emoji.PoliceOfficer,
+                        "cripes" => Emoji.AstonishedFace,
+                        "dark_sunglasses" => Emoji.Sunglasses,
+                        "japanese_goblin" => Emoji.Goblin,
+                        "joy" => Emoji.FaceWithTearsOfJoy,
+                        "mag" => Emoji.MagnifyingGlassTiltedLeft,
+                        "man-bowing" => Emoji.Bowling,
+                        "metal" or "the_horns" => Emoji.SignOfTheHorns,
+                        "muscle" => Emoji.FlexedBiceps,
+                        "nallears" => Emoji.Ear,
+                        "party_parrot" => Emoji.Parrot,
+                        "rip" => Emoji.Headstone,
+                        "sob" => Emoji.LoudlyCryingFace,
+                        "slick" => Emoji.SmilingFaceWithSunglasses,
+                        "smile" or "simple_smile" => Emoji.SmilingFace,
+                        "smirk" or "vee" => Emoji.SmirkingFace,
+                        "smith" => Emoji.Hammer,
+                        "ssh" => Emoji.ShushingFace,
+                        "stuck_out_tongue_winking_eye" => Emoji.WinkingFaceWithTongue,
+                        "suspect" => Emoji.FaceWithRaisedEyebrow,
+                        "tada" => Emoji.MagicWand,
+                        "toot" => Emoji.PartyingFace,
+                        "trollin" => Emoji.GrinningSquintingFace,
+                        "wave" => Emoji.WavingHand,
+                        "white_check_mark" => Emoji.CheckBoxWithCheck,
+                        _ => slackReaction,
+                    };
+                }
+
+                if (
+                    !string.IsNullOrWhiteSpace(result) &&
+                    result.Equals(slackReaction)
+                ) {
                     s_logger.Warning("No Emoji found for slackReaction[{slackReaction}]", slackReaction);
                 }
             }
